c14ddaa27e3af4f3bb9028ad368c388c422f3843
PolAleks <aa.polushkin@yandex.ru>
2025-08-27 13:58:42 +0300

    Удалил из RandomPointBall инициализацию скорости и направления движения мячика

--- BallGames.Common/RandomPointBall.cs
+++ BallGames.Common/RandomPointBall.cs
@@ -6,15 +6,11 @@ namespace Balls.Common
 {
     public class RandomPointBall : Ball
     {
-        private static Random rand = new Random();
-        
+        protected static Random rand = new Random();
         public RandomPointBall(Form form) : base(form)
         {
             centerX = rand.Next(LeftSide, RightSide);
             centerY = rand.Next(TopSide, BottomSide);
-
-            vx = rand.Next(-5, 6);
-            vy = rand.Next(-5, 6);
         }
     }
 }

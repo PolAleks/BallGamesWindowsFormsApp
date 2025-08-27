cf08e61a34d17c219e584f2b3f5b520c02ba7e89
PolAleks <aa.polushkin@yandex.ru>
2025-08-27 19:48:11 +0300

    Исправил процесс движения шариком в методе Go(0

--- BallGames.Common/BilliardBall.cs
+++ BallGames.Common/BilliardBall.cs
@@ -38,21 +38,25 @@ namespace BallGames.Common
             base.Go();
             if (centerX <= LeftSide)
             {
+                centerX = LeftSide;
                 vx = -vx;
                 OnHited?.Invoke(this, new HitEventArgs(Side.Left));
             }
             if (centerX >= RightSide)
             {
+                centerX = RightSide;
                 vx = -vx;
                 OnHited?.Invoke(this, new HitEventArgs(Side.Right));
             }
             if (centerY <= TopSide)
             {
+                centerY = TopSide;
                 vy = -vy;
                 OnHited?.Invoke(this, new HitEventArgs(Side.Top));
             }
             if (centerY >= BottomSide)
             {
+                centerY = BottomSide;
                 vy = -vy;
                 OnHited?.Invoke(this, new HitEventArgs(Side.Bottom));
             }

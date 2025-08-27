3579594d61da2f2d46a9be4f87f707be58c700ce
PolAleks <aa.polushkin@yandex.ru>
2025-08-27 15:18:34 +0300

    Реализовал обработчик события возникающего при столкновении шарика с границей формы

--- BilliardBallsWindowsFormsApp/MainForm.Designer.cs
+++ BilliardBallsWindowsFormsApp/MainForm.Designer.cs
@@ -28,22 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.topLabel = new System.Windows.Forms.Label();
+            this.bottomLabel = new System.Windows.Forms.Label();
+            this.leftLabel = new System.Windows.Forms.Label();
+            this.rightLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // topLabel
+            // 
+            this.topLabel.AutoSize = true;
+            this.topLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.topLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.topLabel.Location = new System.Drawing.Point(393, 9);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(15, 16);
+            this.topLabel.TabIndex = 0;
+            this.topLabel.Text = "0";
+            // 
+            // bottomLabel
+            // 
+            this.bottomLabel.AutoSize = true;
+            this.bottomLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bottomLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.bottomLabel.Location = new System.Drawing.Point(393, 575);
+            this.bottomLabel.Name = "bottomLabel";
+            this.bottomLabel.Size = new System.Drawing.Size(15, 16);
+            this.bottomLabel.TabIndex = 1;
+            this.bottomLabel.Text = "0";
+            // 
+            // leftLabel
+            // 
+            this.leftLabel.AutoSize = true;
+            this.leftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.leftLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.leftLabel.Location = new System.Drawing.Point(12, 289);
+            this.leftLabel.Name = "leftLabel";
+            this.leftLabel.Size = new System.Drawing.Size(15, 16);
+            this.leftLabel.TabIndex = 2;
+            this.leftLabel.Text = "0";
+            // 
+            // rightLabel
+            // 
+            this.rightLabel.AutoSize = true;
+            this.rightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rightLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.rightLabel.Location = new System.Drawing.Point(773, 289);
+            this.rightLabel.Name = "rightLabel";
+            this.rightLabel.Size = new System.Drawing.Size(15, 16);
+            this.rightLabel.TabIndex = 3;
+            this.rightLabel.Text = "0";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.rightLabel);
+            this.Controls.Add(this.leftLabel);
+            this.Controls.Add(this.bottomLabel);
+            this.Controls.Add(this.topLabel);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Бильярд";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Label bottomLabel;
+        private System.Windows.Forms.Label leftLabel;
+        private System.Windows.Forms.Label rightLabel;
     }
 }
 
--- BilliardBallsWindowsFormsApp/MainForm.cs
+++ BilliardBallsWindowsFormsApp/MainForm.cs
@@ -27,8 +27,29 @@ namespace BilliardBallsWindowsFormsApp
             for (int i = 0; i < balls.Capacity; i++)
             {
                 var ball = new BilliardBall(this);
-                balls.Add(ball);
                 ball.Start();
+                ball.OnHited += Ball_OnHited;
+
+                balls.Add(ball);
+            }
+        }
+
+        private void Ball_OnHited(object sender, HitEventArgs e)
+        {
+            switch (e.Side)
+            {
+                case Side.Left: 
+                    leftLabel.Text = (Convert.ToInt32(leftLabel.Text) + 1).ToString();
+                    break;
+                case Side.Right:
+                    rightLabel.Text = (Convert.ToInt32(rightLabel.Text) + 1).ToString();
+                    break;
+                case Side.Top:
+                    topLabel.Text = (Convert.ToInt32(topLabel.Text) + 1).ToString();
+                    break;
+                case Side.Bottom:
+                    bottomLabel.Text = (Convert.ToInt32(bottomLabel.Text) + 1).ToString();
+                    break;
             }
         }
     }

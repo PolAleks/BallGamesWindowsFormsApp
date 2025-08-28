0ad0eab92d7b074314dcdd472e504831f7ad6fc1
PolAleks <aa.polushkin@yandex.ru>
2025-08-28 11:39:39 +0300

    Создал базовую модель шарика SaluteBall

---
+++ BallGames.Common/SaluteBall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Balls.Common
+{
+    public class SaluteBall : MoveBall
+    {
+        private float g = 0.2f;
+        public SaluteBall(Form form, float centerX, float centerY) : base(form)
+        {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            radius = 10;
+            vy = -Math.Abs(vy);
+        }
+
+        protected override void Go()
+        {
+            base.Go();
+            vy += g;
+        }
+    }
+}

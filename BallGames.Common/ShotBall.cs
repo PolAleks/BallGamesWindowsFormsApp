891d292d88fa7dbff78763b4c84efa4eed848cf4
PolAleks <aa.polushkin@yandex.ru>
2025-08-29 13:26:24 +0300

    Изменил уровень доступа для метода Clear() модели Ball с private на protected

---
+++ BallGames.Common/ShotBall.cs
@@ -0,0 +1,43 @@
+﻿using Balls.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallGames.Common
+{
+    public class ShotBall : MoveBall
+    {
+        private float g = 0.1f;
+        public event EventHandler<BoomEventArgs> OnBaDaBoom;
+        public ShotBall(Form form) : base(form)
+        {
+            centerY = BottomSide;
+            radius = rand.Next(2, 5);
+            vy = -Math.Abs(vy);
+        }
+
+        protected override void Go()
+        {
+            base.Go();
+            vy += g;
+            if (vy <= 0)
+            {
+                Stop();
+                Clear();
+                OnBaDaBoom?.Invoke(this, new BoomEventArgs(rand.Next(7, 16)));
+            }
+        }
+    }
+
+    public class BoomEventArgs : EventArgs
+    {
+        public int Count { get; }
+        public BoomEventArgs(int count)
+        {
+            Count = count;
+        }
+    }
+}

de1f09d6a7c5f4d221aef379e0881bcf00104c9b
PolAleks <aa.polushkin@yandex.ru>
2025-08-31 16:37:28 +0300

    Вынес в отдельные методы подсчет сбитых мячиков и поражение мячика

--- FruitNinjaWinFormApp/MainForm.Designer.cs
+++ FruitNinjaWinFormApp/MainForm.Designer.cs
@@ -28,24 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.hitBallLabel = new System.Windows.Forms.Label();
+            this.hitBallValueLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // hitBallLabel
+            // 
+            this.hitBallLabel.AutoSize = true;
+            this.hitBallLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hitBallLabel.Location = new System.Drawing.Point(656, 50);
+            this.hitBallLabel.Name = "hitBallLabel";
+            this.hitBallLabel.Size = new System.Drawing.Size(105, 16);
+            this.hitBallLabel.TabIndex = 0;
+            this.hitBallLabel.Text = "Сбито мячей:";
+            // 
+            // hitBallValueLabel
+            // 
+            this.hitBallValueLabel.AutoSize = true;
+            this.hitBallValueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hitBallValueLabel.Location = new System.Drawing.Point(785, 50);
+            this.hitBallValueLabel.Name = "hitBallValueLabel";
+            this.hitBallValueLabel.Size = new System.Drawing.Size(15, 16);
+            this.hitBallValueLabel.TabIndex = 1;
+            this.hitBallValueLabel.Text = "0";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.hitBallValueLabel);
+            this.Controls.Add(this.hitBallLabel);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Fruit Ninja";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label hitBallLabel;
+        private System.Windows.Forms.Label hitBallValueLabel;
     }
 }
 
--- FruitNinjaWinFormApp/MainForm.cs
+++ FruitNinjaWinFormApp/MainForm.cs
@@ -1,12 +1,64 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Balls.Common;
 
 namespace FruitNinjaWinFormApp
 {
     public partial class MainForm : Form
     {
+        private List<Ball> balls;
+        private Timer timer;
+        private int countHitBall = 0;
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {            
+            InitialGame();
+        }
+
+        private void InitialGame()
+        {
+            balls = new List<Ball>();
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            var ball = new ShotBallRandomSize(this);
+            balls.Add(ball);
+            ball.Start();
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            foreach (var ball in balls)
+            {
+                if (ball.Contains(e.Location))
+                {
+                    OnHitBall(ball);
+                    UpdateCountHitBall();
+
+                    break;
+                }
+            }
+        }
+
+        private void UpdateCountHitBall() => hitBallValueLabel.Text = (++countHitBall).ToString();
+
+        private void OnHitBall(Ball ball)
+        {
+            ball.Stop();
+            ball.Clear();
+            balls.Remove(ball);
         }
     }
 }
